1de5ca9a7bfecc1ffb30fbd322ef5ee2c5f60afe
Hieu Huynh <hieuhuynhif@gmail.com>
2023-04-30 22:18:31 +0700

    30/4/23

--- Ass-2/restaurant.cpp
+++ Ass-2/restaurant.cpp
@@ -5,7 +5,11 @@ struct fStringNode{
 		int fre;
 		fStringNode *left, *right;
 };
-
+struct Customer{
+	int id = -1;
+	int result = -1;
+	int num = -1;
+};
 fStringNode* createHuffmanTree(fStringNode *&root,fStringNode ** arrayNode, int &size, int current)
 {
 	if(current == size-1)
@@ -60,11 +64,48 @@ void pathStr(fStringNode *root, string str, string current, string &path)
 		pathStr(root->right, str, current+"1", path);
 	}
 }
+bool addSea(Customer *&seaList, int &result)
+{
+		int id = result%MAXSIZE + 1;
+		int index = result%(MAXSIZE/2);
+		if(seaList[index].id == -1)
+		{
+			seaList[index].id = id;
+			seaList[index].result = result;
+			seaList[index].num = id;
+			return true;
+		}
+		else
+		{
+			int current = index + 1;
+			while(current != index)
+			{
+				if(seaList[current].id == -1)
+				{
+					seaList[current].id = id;
+					seaList[current].result = result;
+					seaList[current].num = id;
+					return true;
+				}
+				current++;
+				if(current == MAXSIZE/2)
+				{
+					current = 0;
+				}
+			}
+			return false;
+		}
+}
+bool addMountain(int &result)
+{
+	int id = result%MAXSIZE + 1;
 
-void REG(string &token, string &name, int &iD, int &maxSize, int &result)
+}
+void REG(string &token, Customer * seaList, int &seaSize, int &mountainSize)
 {
 	int count[150];
 	int size = 0;
+	int result;
 	for(int i = 0; i< 150; i++)
 	{
 		count[i] = 0;
@@ -122,7 +163,48 @@ void REG(string &token, string &name, int &iD, int &maxSize, int &result)
 		pathStr(root, str, "", path);
 		binaryCode += path;
 	}
-	cout<<binaryCode;
+	if(token.length() == 1)
+	{
+		binaryCode = "1";
+	}
+	else if(binaryCode.length() > 15)
+	{
+		binaryCode = binaryCode.substr(binaryCode.length()-15, 15);
+	}
+	result = 0;
+	for(int i = 0; i<binaryCode.length(); i++)
+	{
+		if(binaryCode[i] == 48)
+		{
+			result = result*2 + 0;
+		}
+		else
+		{
+			result = result*2 + 1;
+		}
+	}
+
+	if(result % 2)
+	{
+		if(!addSea(seaList, result))
+		{
+			if(!addMountain(result))
+			{
+				
+			}
+		}
+	}
+	else
+	{
+		if(!addMountain(result))
+		{
+			if(!addSea(seaList, result))
+			{
+
+			}
+		}
+
+	}
 	
 }
 
@@ -145,13 +227,10 @@ cout<<"mh";
 
 void simulate(string filename)
 {
-	string name;
-	int num;
-	int iD;
-	int maxSize;
-	int result;
+	Customer seaList[MAXSIZE/2];
+	int seaSize = 0;
+	int mountainSize = 0;
 	string token;
-
 	string method, strLine;
 	ifstream MyReadFile(filename);
 	while(getline(MyReadFile, strLine))
@@ -161,7 +240,7 @@ void simulate(string filename)
 		token = strLine;
 		if(method == "REG")
 		{
-			REG(token, name, iD, maxSize, result);
+			REG(token, seaList, seaSize, mountainSize);
 		}
 		else if(method == "CLE")
 		{
