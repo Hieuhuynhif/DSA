f35fe679d97864bc122bf34a1f74de1c5c240299
Hieuhuynhif <hieuhuynhif@gmail.com>
2024-03-05 13:06:10 +0700

    init

---
+++ Ass1/kNN.cpp
@@ -0,0 +1,12 @@
+#include "kNN.hpp"
+
+/* TODO: You can implement methods, functions that support your data structures here.
+ * */
+Dataset::Dataset()
+{
+    data = nullptr;
+}
+void Dataset::columns() const
+{
+    cout << "test function";
+}
